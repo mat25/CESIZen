9fc1b2459438deecba141fb20288f2d94867d3e7
mat25 <mateojean25660@gmail.com>
2025-06-01 10:16:53 +0200

    Ajout # dans la vérification du mot de passe lors du register

--- src/main/java/com/CESIZen/prod/dto/resetPassword/ResetPasswordConfirmDTO.java
+++ src/main/java/com/CESIZen/prod/dto/resetPassword/ResetPasswordConfirmDTO.java
@@ -10,7 +10,7 @@ public class ResetPasswordConfirmDTO {
 
     @NotBlank(message = "Le nouveau mot de passe ne peut pas être vide")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#@$!%*?&])[A-Za-z\\d#@$!%*?&]{8,}$",
             message = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial"
     )
     private String newPassword;
--- src/main/java/com/CESIZen/prod/dto/user/RegisterDTO.java
+++ src/main/java/com/CESIZen/prod/dto/user/RegisterDTO.java
@@ -14,7 +14,7 @@ public class RegisterDTO {
 
     @NotBlank(message = "Le mot de passe ne peut pas être vide")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#@$!%*?&])[A-Za-z\\d#@$!%*?&]{8,}$",
             message = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial"
     )
     private String password;
